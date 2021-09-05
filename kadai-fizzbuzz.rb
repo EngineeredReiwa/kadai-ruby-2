092905f77ce24f917069fab959ac70416d64b365
EngineeredReiwa <k.keisuke.1230@gmail.com>
2021-09-05 15:19:43 +0000

    2nd commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,5 +1,4 @@
 def fizzbuzz(num)
-  # 内容は省略。自力で考えてみましょう。
   if(num%3 == 0 && num%5 == 0)
     return "FizzBuzz"
   elsif(num%3 == 0 && num%5 != 0)
@@ -16,9 +15,3 @@ num_max = 100
 (1..num_max).each do |num|
   puts fizzbuzz(num)
 end
-
-# 以下は動作確認用の記述です
-puts fizzbuzz(1)
-puts fizzbuzz(3)
-puts fizzbuzz(5)
-puts fizzbuzz(15)
